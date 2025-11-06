e658c2a4e293bda2445c37cd0b914a8ffc2f4566
Athil khan <athilkhan2005@gmail.com>
2025-11-06 14:09:38 +0530

    v1

---
+++ ai_model/face/model.py
@@ -0,0 +1,9 @@
+import cv2 
+
+im = cv2.imread("C:/Users/athin/OneDrive/Pictures/Screenshots/Screenshot 2025-11-01 190826.png")
+
+cv2.imshow("summa", im)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
