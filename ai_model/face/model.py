65ae798da0f8be874309d1d1c18ac5eebecc8784
Athil khan <athilkhan2005@gmail.com>
2025-11-08 10:35:43 +0530

    v1

--- ai_model/face/model.py
+++ ai_model/face/model.py
@@ -1,9 +1,21 @@
-import cv2 
+import cv2
 
-im = cv2.imread("C:/Users/athin/OneDrive/Pictures/Screenshots/Screenshot 2025-11-01 190826.png")
+alg = "haarcascade_frontalface_default.xml"
+model = cv2.CascadeClassifier(alg)
 
-cv2.imshow("summa", im)
+img = cv2.imread("./detect.jpg", cv2.IMREAD_GRAYSCALE)
 
-cv2.waitKey(0)
+faces = model.detectMultiScale(
+    img,
+    scaleFactor=1.05,
+    minNeighbors=2,
+    minSize=(100, 100)
+)
 
-cv2.destroyAllWindows()
+for x, y, w, h in faces:
+    cropped_image = img[y : y + h, x : x + w]
+    target_file_name = 'face.jpg'
+    cv2.imwrite(
+        target_file_name,
+        cropped_image,
+    )
