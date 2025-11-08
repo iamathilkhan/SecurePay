6c703e8eeb12cabb994af50548b62fb6c64b2d6a
Athil khan <athilkhan2005@gmail.com>
2025-11-08 17:02:00 +0530

    v1

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-from app.blueprints.verify import verify_bp
-from app.blueprints.wallet import wallet_bp
-from app.blueprints.auth import auth_bp
-from flask import Flask, render_template
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def home():
-        return render_template('base.html')
-
-    app.register_blueprint(verify_bp, url_prefix='/verify')
-    app.register_blueprint(wallet_bp, url_prefix='/wallet')
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-
-    return app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug = True)
+    app.run(debug=True)
---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from .blueprints.verify import verify_bp
+from .blueprints.wallet import wallet_bp
+from .blueprints.auth import auth_bp
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('/base.html')
+
+    app.register_blueprint(verify_bp, url_prefix='/verify')
+    app.register_blueprint(wallet_bp, url_prefix='/wallet')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
