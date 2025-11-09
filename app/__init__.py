52a9f863a8664055f423bf274832a765eb6f9d69
Athil khan <athilkhan2005@gmail.com>
2025-11-09 16:47:53 +0530

    v1DB

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,22 @@ from .blueprints.verify import verify_bp
 from .blueprints.wallet import wallet_bp
 from .blueprints.auth import auth_bp
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users'
+
+    db = SQLAlchemy(app=app)
+
+    class Users(db.Model):
+        id = db.Column(db.Integer, primary_key = True)
+        name = db.Column(db.String, nullable = False)
+        email = db.Column(db.String, nullable = False)
+
+        def __repr__(self):
+            return '<Name %r>'% self.name
 
     @app.route('/')
     def home():
