b0d4d00d7dc0af33a677d343f686ad5d1fe89867
Athil khan <athilkhan2005@gmail.com>
2025-11-16 16:09:12 +0530

    DataBase Creation

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,20 @@ from .blueprints.wallet import wallet_bp
 from .blueprints.auth import auth_bp
 from flask import Flask, render_template
 from .extension import db
+import os
+
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'SecurePay.db')
 
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     @app.route('/login')
     def login():
